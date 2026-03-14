bec148509f24f0491650c0c616c015e32d3446a8
Jeremia Blut <jeremia@ulcl.de>
2026-03-14 13:08:43 +0100

    Added new features

--- src/client/java/com/eclipsew/EclipseClient.java
+++ src/client/java/com/eclipsew/EclipseClient.java
@@ -13,7 +13,7 @@ import java.awt.*;
 public class EclipseClient implements ClientModInitializer {
 	private Minecraft minecraft;
 	private Player player;
-	private boolean fps = false;
+	private boolean fps = false, sprint = true, sneak = false;
 	@Override
 	public void onInitializeClient() {
 		ClientTickEvents.END_CLIENT_TICK.register((minecraft_ -> {
@@ -21,19 +21,46 @@ public class EclipseClient implements ClientModInitializer {
 			minecraft = minecraft_;
 			Integer ifps = minecraft.getFps();
 			Color color = Color.CYAN;
-			if (fps && player != null) {
-				net.minecraft.network.chat.Component component = Component.literal(String.valueOf(ifps))
+			if (player != null) {
+				if (fps) {
+					Component component = Component.literal(String.valueOf(ifps))
 						.withColor(color.getRGB()).withoutShadow();
-				player.displayClientMessage(component, true);
+					player.displayClientMessage(component, true);
+				}
+				if (sprint) {
+					player.setSprinting(true);
+				}
+				if (sneak) {
+					player.setShiftKeyDown(true);
+				}
 			}
 		}));
 
 		ClientCommandRegistrationCallback.EVENT.register((dispatcher, registryAccess) -> {
 			dispatcher.register(ClientCommandManager.literal("toggle.fps").executes(context -> {
                 fps = !fps;
+				Component component = Component.literal("FPS toggled to " + fps);
+				player.displayClientMessage(component, false);
 				return 1;
 			}));
 		});
 
+		ClientCommandRegistrationCallback.EVENT.register((dispatcher, registryAccess) -> {
+			dispatcher.register(ClientCommandManager.literal("toggle.sprint").executes(context -> {
+				sprint = !sprint;
+				Component component = Component.literal("SPRINT toggled to " + sprint);
+				player.displayClientMessage(component, false);
+				return 1;
+			}));
+		});
+
+		ClientCommandRegistrationCallback.EVENT.register((dispatcher, registryAccess) -> {
+			dispatcher.register(ClientCommandManager.literal("toggle.sneak").executes(context -> {
+				sneak = !sneak;
+				Component component = Component.literal("SNEAK toggled to " + sneak);
+				player.displayClientMessage(component, false);
+				return 1;
+			}));
+		});
 	}
 }
